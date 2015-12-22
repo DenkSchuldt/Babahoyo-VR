f00b408939eaa2b7bfceda9f8d7c4d8ab4c4908e
Denny Schuldt <dschuldt@outlook.com>
2015-12-21 23:26:39 -0500

    Lerping

--- Assets/Cardboard/Scripts/South.cs
+++ Assets/Cardboard/Scripts/South.cs
@@ -6,10 +6,12 @@ public class South : MonoBehaviour {
 	private GameObject fade;
 	private CardboardHead head;
 	private Vector3 startingPosition;
-	private float delay = 0.0f; 
-	//private Gyroscope gyro;
-
+	private float delay;
+	private float lerp;
+	
 	void Start() {
+		delay = 0.0f;
+		lerp = 0;
 		fade = GameObject.Find ("Fade");
 		head = Camera.main.GetComponent<StereoController>().Head;
 		startingPosition = transform.localPosition;
@@ -20,9 +22,11 @@ public class South : MonoBehaviour {
 		bool isLookedAt = GetComponent<Collider>().Raycast(head.Gaze, out hit, Mathf.Infinity);
 		if (!isLookedAt) { 
 			delay = Time.time + 2.0f;
+			lerp = 0;
 			gameObject.GetComponent<Renderer>().material.color = Color.white;
 		} else {
-			gameObject.GetComponent<Renderer>().material.color = Color.green;
+			lerp += Time.deltaTime;
+			gameObject.GetComponent<Renderer>().material.color = Color.Lerp(Color.white, Color.green, lerp/2);
 		}
 		if ((Cardboard.SDK.CardboardTriggered && isLookedAt) || (isLookedAt && Time.time>delay)) {
 			float fadeTime = fade.GetComponent<Fade>().BeginFade(1);
